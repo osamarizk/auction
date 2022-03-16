8b706949d63b28e3057117a4ef590f72a4cada43
Osama Rizk <osamarizk20@gmail.com>
2022-03-16 23:40:28 +0200

    modification to truffle.config

--- truffle-config.js
+++ truffle-config.js
@@ -78,7 +78,7 @@ module.exports = {
     // timeout: 100000
   },
 
-  // Configure your compilers
+  // Configure your compilers.
   compilers: {
     solc: {
       version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
